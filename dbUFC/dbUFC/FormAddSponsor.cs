0fb4850b6b371defe194117ff9fda541ce2120a4
Petru Potrimba <petru.potrimba@studio.unibo.it>
2017-08-01 01:13:25 +0200

    Solved problem of comparing two strings: database give me the data with the a costant (50) length. This length is the length of attributes of entities of database. Also removed some unless image in resand added one new

--- dbUFC/dbUFC/FormAddSponsor.cs
+++ dbUFC/dbUFC/FormAddSponsor.cs
@@ -34,12 +34,36 @@ namespace dbUFC
             this.dc = new dbUFCDataContext();
             Sponsor spo = new Sponsor();
             List<Sponsor> ls = dc.Sponsors.ToList();
-            string str = (string) bunifuTextbox1.text;
-            
+            /*   List<string> str = new List<string>();
+               foreach(var v in ls)
+               {
+                   str.Add(v.NomeSposor.ToString());
+                   MessageBox.Show(v.NomeSposor.ToString());
+               }
+               MessageBox.Show(""+str[0].Length);
+               string str1 = bunifuTextbox1.text;
+               MessageBox.Show(str1);
+               foreach ( string s in str)
+               {
+                   if(s == str1)
+                   {
+                       MessageBox.Show("ok");
+                   }
+               }
+               this.dc.Sponsors.InsertOnSubmit(spo);
+               this.dc.SubmitChanges();
+               MessageBox.Show("The new sponsor has been added correctly.");
+               */
+            spo.NomeSposor = bunifuTextbox1.text;
+            //string trimmed = "careteam order4-26-11.csv".Replace(" ", string.Empty);
+            string trim = ls[0].NomeSposor.Replace(" ", string.Empty);
+            MessageBox.Show("" + trim.Length);
 
-            /*this.dc.Sponsors.InsertOnSubmit(spo);
-            this.dc.SubmitChanges();
-            MessageBox.Show("The new sponsor has been added correctly.");*/
+            if(spo.NomeSposor == trim)
+            {
+                MessageBox.Show("Ok!!!");
+            }
+
 
 
         }
